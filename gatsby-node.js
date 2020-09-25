eefa925c1847606ca641ea36fee6546ac015046d
Igor Koptev <igor.koptef@gmail.com>
2020-09-25 16:33:18 +0300

    Page creation with template hierarchy

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,87 @@
 const _ = require("lodash")
-const path = require("path")
+const fs = require("fs")
+const { resolve } = require("path")
+const url = require("url")
 const { createFilePath } = require("gatsby-source-filesystem")
 const { fmImagesToRelative } = require("gatsby-remark-relative-images")
 const { dd, dump } = require("dumper.js")
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
-    const result = await graphql(`
+    const {
+        data: {
+            allMarkdownRemark: { nodes: allPages },
+        },
+    } = await graphql(`
         {
-            frontPage: allMarkdownRemark(
+            allMarkdownRemark(
                 limit: 1000
-                filter: { fields: { slug: { eq: "/front-page/" } } }
+                filter: { fields: { slug: { ne: "/meta-page/" } } }
             ) {
-                edges {
-                    node {
-                        id
+                nodes {
+                    id
+                    fields {
+                        slug
                     }
                 }
             }
         }
     `)
-}
 
+    //https://gist.github.com/jens1101/9f3faa6c2dae23537257f1c3d0afdfdf
+    function removeTrailingSlashes(url) {
+        return url.replace(/\/+$/, "") //Removes one or more trailing slashes from URL
+    }
+    const pagesWithoutFrontPage = allPages.filter(
+        (page) => page.fields.slug !== "/front-page/"
+    )
+    pagesWithoutFrontPage.map(async (page, i) => {
+        const id = page.id
+
+        // ---------------- Just for fun -----------------
+        const someSlug = "/abc/def/"
+            .split("/") // ["", "abc", "def", ""]
+            .filter((el) => el) //  ["abc", "def"] - Because an empty string evaluates to boolean false. It works with all falsy values like 0, false, null, undefined, '', etc.
+            .slice(-1)[0] // last element of array
+        // ---------------- Just for fun -----------------
+
+        const isFrontPage = page.fields.slug === "/"
+
+        const frontPageTemplate = resolve(`src/templates/front-page.js`)
+        const existsFrontPageTemplate = fs.existsSync(frontPageTemplate)
+
+        const customTemplate = resolve(
+            `src/templates/${removeTrailingSlashes(page.fields.slug)}.js`
+        )
+        const existsCustomTemplate = fs.existsSync(customTemplate)
+
+        const defaultTemplate = resolve(`src/templates/default-page.js`)
+
+        var actualTemplate = null
+
+        switch (true) {
+            case isFrontPage && existsFrontPageTemplate:
+                actualTemplate = frontPageTemplate
+                break
+
+            case existsCustomTemplate:
+                actualTemplate = customTemplate
+                break
+
+            default:
+                actualTemplate = defaultTemplate
+        }
+
+        actions.createPage({
+            path: page.fields.slug,
+            component: actualTemplate,
+            // additional data can be passed via context
+            context: {
+                id: id,
+                slug: page.fields.slug,
+            },
+        })
+    })
+}
 exports.onCreateNode = ({ node, actions, getNode }) => {
     const { createNodeField } = actions
     fmImagesToRelative(node) // convert image paths for gatsby images
@@ -33,9 +94,4 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
             value,
         })
     }
-    // if (node.internal.type === `ImageSharp`) {
-    //     dump("######################################")
-    //     dump(node)
-    //     dump("######################################")
-    // }
 }
