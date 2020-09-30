1dd9886defb709268f637e2e29904c67d6c8f8b8
Igor Koptev <igor.koptef@gmail.com>
2020-09-30 16:56:54 +0300

    Moved config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,13 +33,6 @@ module.exports = {
         {
             resolve: "gatsby-source-filesystem",
             options: {
-                path: `${__dirname}/settings/site-metadata.md`,
-                name: "meta",
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
                 path: `${__dirname}/src/img`,
                 name: "images",
             },
