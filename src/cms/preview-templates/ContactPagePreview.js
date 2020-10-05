5f86bacac02e31d40436f18d033f534036ce7e7e
Igor Koptev <igor.koptef@gmail.com>
2020-10-05 09:14:31 +0300

    Page fefactor

---
+++ src/cms/preview-templates/ContactPagePreview.js
@@ -0,0 +1,72 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { ContactsPageViewTemplate } from "../../views/ContactsPageView/ContactsPageView"
+import TopLayout from "../../../plugins/gatsby-plugin-top-layout/TopLayout"
+
+const ProductPagePreview = ({ entry, getAsset }) => {
+    // const entryBlurbs = entry.getIn(["data", "intro", "blurbs"])
+    // const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
+
+    // const entryTestimonials = entry.getIn(["data", "testimonials"])
+    // const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
+
+    // const entryPricingPlans = entry.getIn(["data", "pricing", "plans"])
+    // const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
+
+    return (
+        <TopLayout>
+            <ContactsPageViewTemplate
+                header={entry.getIn(["data", "contactsHeading", "ru"])}
+                emailLabel="EMail"
+                email="test@local.mail"
+                addressLabel="Address"
+                address="<h4>Moscow</h4><p>Bolshoi Konushennij 4</p>"
+                phoneNumberLabel="Phone Number"
+                phone="123456789"
+                // image={entry.getIn(["data", "image"])}
+                // title={entry.getIn(["data", "title"])}
+                // heading={entry.getIn(["data", "heading"])}
+                // description={entry.getIn(["data", "description"])}
+                // intro={{ blurbs }}
+                // main={{
+                //     heading: entry.getIn(["data", "main", "heading"]),
+                //     description: entry.getIn(["data", "main", "description"]),
+                //     image1: {
+                //         image: getAsset(
+                //             entry.getIn(["data", "main", "image1", "image"])
+                //         ),
+                //         alt: entry.getIn(["data", "main", "image1", "alt"]),
+                //     },
+                //     image2: {
+                //         image: getAsset(
+                //             entry.getIn(["data", "main", "image2", "image"])
+                //         ),
+                //         alt: entry.getIn(["data", "main", "image2", "alt"]),
+                //     },
+                //     image3: {
+                //         image: getAsset(
+                //             entry.getIn(["data", "main", "image3", "image"])
+                //         ),
+                //         alt: entry.getIn(["data", "main", "image3", "alt"]),
+                //     },
+                // }}
+                // fullImage={entry.getIn(["data", "full_image"])}
+                // testimonials={testimonials}
+                // pricing={{
+                //     heading: entry.getIn(["data", "pricing", "heading"]),
+                //     description: entry.getIn(["data", "pricing", "description"]),
+                //     plans: pricingPlans,
+                // }}
+            />
+        </TopLayout>
+    )
+}
+
+ProductPagePreview.propTypes = {
+    entry: PropTypes.shape({
+        getIn: PropTypes.func,
+    }),
+    getAsset: PropTypes.func,
+}
+
+export default ProductPagePreview
