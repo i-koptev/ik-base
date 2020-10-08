9db65293ade51434598cf849e6f4cfc8e79db995
Igor Koptev <igor.koptef@gmail.com>
2020-10-08 10:45:24 +0300

    SidebarNav refactor

--- src/layouts/Main/components/Sidebar/components/sidebarNav/SidebarNav.js
+++ src/layouts/Main/components/Sidebar/components/sidebarNav/SidebarNav.js
@@ -1,19 +1,17 @@
 /* eslint-disable react/no-multi-comp */
 /* eslint-disable react/display-name */
 import React, { forwardRef } from "react"
-import { graphql, useStaticQuery } from "gatsby"
 
-import {
-    changeLocale,
-    injectIntl,
-    Link,
-    FormattedMessage,
-} from "gatsby-plugin-intl"
+import { injectIntl, Link } from "gatsby-plugin-intl"
 
 import clsx from "clsx"
 import PropTypes from "prop-types"
+
 import { makeStyles } from "@material-ui/styles"
-import { List, ListItem, Button } from "@material-ui/core"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
 
 import DashboardIcon from "@material-ui/icons/Dashboard"
 import PeopleIcon from "@material-ui/icons/People"
@@ -27,25 +25,16 @@ import LockOpenIcon from "@material-ui/icons/LockOpen"
 // import MenuItem from "./MenuItem"
 
 const useStyles = makeStyles((theme) => ({
-    root: {},
-    item: {
-        display: "flex",
-        paddingTop: 0,
-        paddingBottom: 0,
-        "&:hover": {
-            // backgroundColor: "#fee",
-        },
+    root: {
+        flexGrow: 1,
+        // backgroundColor: "red",
     },
-    button: {
-        display: "flex",
+    link: {
         color: theme.layouts.Main.SidebarNav.MenuItem.fontColor,
-        padding: "10px 8px",
-        justifyContent: "flex-start",
-        textTransform: "none",
-        letterSpacing: 0.1,
-        width: "100%",
-        fontWeight: theme.layouts.Main.SidebarNav.MenuItem.fontWeight,
         textDecoration: "none",
+    },
+    listItem: {
+        padding: "7px 10px",
         "&:hover": {
             backgroundColor:
                 theme.layouts.Main.SidebarNav.MenuItem.hoverBackgroundColor,
@@ -55,9 +44,15 @@ const useStyles = makeStyles((theme) => ({
             color: theme.layouts.Main.SidebarNav.MenuItem.iconColor,
             width: "18px",
             height: "18px",
-            transform: "translateY(4px)",
-            marginRight: "12px",
+            // transform: "translateY(1px)",
+            // marginRight: "22px",
         },
+    },
+    listItemText: {
+        marginLeft: "-15px",
+        fontWeight: theme.layouts.Main.SidebarNav.MenuItem.fontWeight,
+        // marginBottom: "-7px",
+        // marginTop: "7px",
     },
     active: {
         color: theme.layouts.Main.SidebarNav.MenuItem.activeFontColor,
@@ -67,18 +62,6 @@ const useStyles = makeStyles((theme) => ({
         },
     },
 }))
-
-const CustomLink = forwardRef((props, ref) => (
-    <div ref={ref} style={{ flexGrow: 1 }}>
-        <Link {...props} />
-    </div>
-))
-
-const OuterLink = forwardRef((props, ref) => (
-    <div ref={ref} style={{ flexGrow: 1 }}>
-        <a href={props.to} {...props} target="_blank" />
-    </div>
-))
 
 const SidebarNav = (props) => {
     const { intl, className, sidebarNavItems, ...rest } = props
@@ -97,56 +80,83 @@ const SidebarNav = (props) => {
     }
 
     return (
-        <List {...rest} className={clsx(classes.root, className)}>
-            {/* {JSON.stringify(sidebarNavItems)} */}
-            {/* {mainNavLinks.map((link) => ( */}
+        // <div className={classes.root}>
+        <List
+            component="nav"
+            {...rest}
+            className={clsx(classes.root, className)}
+        >
             {sidebarNavItems.map((menuItem) => (
-                <ListItem
-                    className={classes.item}
-                    disableGutters
-                    key={`key-${menuItem.slug}`}
-                >
-                    {/* {/^\/(?!\/)/.test(menuItem.slug) ? ( */}
+                <>
                     {/^https?:\/\/(.*)/.test(menuItem.slug) ? (
-                        <Button
-                            // activeClassName={classes.active}
-                            className={classes.button}
-                            component={OuterLink}
-                            to={menuItem.slug}
+                        <a
+                            className={classes.link}
+                            href={menuItem.slug}
+                            target="_blank"
+                            activeClassName={classes.active}
+                            key={`key-${menuItem.slug}`}
                         >
-                            <div className={classes.icon}>
-                                {iconObj[menuItem.icon]}
-                            </div>
-                            {intl.formatMessage({
-                                id: `sidebarMenu.${menuItem.slug.replace(
-                                    /\//g,
-                                    ""
-                                )}.title`,
-                            })}
-                        </Button>
+                            <ListItem
+                                button
+                                className={classes.listItem}
+                                disableGutters
+                                key={`key-${menuItem.slug}`}
+                            >
+                                <ListItemIcon
+                                    style={{
+                                        color: "inherit",
+                                    }}
+                                >
+                                    {iconObj[menuItem.icon]}
+                                </ListItemIcon>
+                                <ListItemText
+                                    className={classes.listItemText}
+                                    primary={intl.formatMessage({
+                                        id: `sidebarMenu.${menuItem.slug.replace(
+                                            /\//g,
+                                            ""
+                                        )}.title`,
+                                    })}
+                                />
+                            </ListItem>
+                        </a>
                     ) : (
-                        <Button
-                            // activeClassName={classes.active}
-                            className={classes.button}
-                            component={CustomLink}
+                        <Link
+                            className={classes.link}
                             to={
                                 menuItem.slug === "/"
                                     ? menuItem.slug
                                     : `/${menuItem.slug.replace(/\//g, "")}/`
                             }
+                            activeClassName={classes.active}
+                            key={`key-${menuItem.slug}`}
                         >
-                            <div className={classes.icon}>
-                                {iconObj[menuItem.icon]}
-                            </div>
-                            {intl.formatMessage({
-                                id: `sidebarMenu.${menuItem.slug.replace(
-                                    /\//g,
-                                    ""
-                                )}.title`,
-                            })}
-                        </Button>
+                            <ListItem
+                                button
+                                className={classes.listItem}
+                                disableGutters
+                                key={`key-${menuItem.slug}`}
+                            >
+                                <ListItemIcon
+                                    style={{
+                                        color: "inherit",
+                                    }}
+                                >
+                                    {iconObj[menuItem.icon]}
+                                </ListItemIcon>
+                                <ListItemText
+                                    className={classes.listItemText}
+                                    primary={intl.formatMessage({
+                                        id: `sidebarMenu.${menuItem.slug.replace(
+                                            /\//g,
+                                            ""
+                                        )}.title`,
+                                    })}
+                                />
+                            </ListItem>
+                        </Link>
                     )}
-                </ListItem>
+                </>
             ))}
         </List>
     )
