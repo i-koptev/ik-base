e350b39ca4b12c8ab923e010d8e6dfdaf709b03e
Igor Koptev <igor.koptef@gmail.com>
2020-10-15 13:15:12 +0300

    Font test - removed font load from TopLayout

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -14,10 +14,18 @@ export default function TopLayout(props) {
                         name="viewport"
                         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
                     />
-                    <link
+                    {/* <link
                         href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
                         rel="stylesheet"
                     />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+                        rel="stylesheet"
+                    ></link>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+                        rel="stylesheet"
+                    ></link> */}
                 </Helmet>
             }
             <ThemeProvider theme={theme}>
