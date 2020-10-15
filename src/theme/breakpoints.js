500ebc3a75e9c27a360519a0bdf978d75fb40d43
Igor Koptev <igor.koptef@gmail.com>
2020-10-15 13:49:26 +0300

    Theme breakpoints changed to defaults

--- src/theme/breakpoints.js
+++ src/theme/breakpoints.js
@@ -2,8 +2,11 @@ export default {
     values: {
         xs: 0,
         sm: 600,
-        md: 784,
-        lg: 1120,
-        xl: 1472,
+        // md: 784,
+        md: 960,
+        // lg: 1120,
+        lg: 1280,
+        // xl: 1472,
+        xl: 1922,
     },
 }
