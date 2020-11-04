d2cf4e8218c9c9950d69cde00716172ded7defdc
Igor Koptev <igor.koptef@gmail.com>
2020-11-04 13:29:35 +0200

    Contact form creation process

--- src/views/ContactsPageView/ContactsPageView.js
+++ src/views/ContactsPageView/ContactsPageView.js
@@ -1,65 +1,148 @@
-import React from "react"
+import React, { useCallback } from "react"
 import clsx from "clsx"
 import PropTypes from "prop-types"
 
+import { injectIntl } from "gatsby-plugin-intl"
+
+import { useForm } from "react-hook-form"
+import { DevTool } from "react-hook-form-devtools"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
 import { makeStyles, useTheme } from "@material-ui/styles"
-import { Grid } from "@material-ui/core"
-import Container from "@material-ui/core/Container"
-import Typography from "@material-ui/core/Typography"
+import {
+    withStyles,
+    Typography,
+    Container,
+    Grid,
+    Button,
+    TextField,
+    Checkbox,
+    FormControlLabel,
+} from "@material-ui/core"
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        // backgroundColor: "#f1f1f1",
         [theme.breakpoints.up("lg")]: {
             // backgroundColor: "tomato",
         },
     },
     header: {
-        padding: "1rem",
+        marginTop: "2rem",
+        marginBottom: "3rem",
     },
-    htmlContent: {
-        "& h1, h2, h3, h4, h5, h6": {
-            textAlign: "center",
+    label: {
+        display: "inline-block",
+        marginTop: "1em",
+        marginBottom: "1em",
+    },
+    // leftCol: { minHeight: "200px", backgroundColor: "#efefef" },
+    rightCol: { border: "1px solid skyblue", width: "100%" },
+    form: {
+        // width: "100%",
+        // border: "1px solid skyblue",
+        // backgroundColor: "rgba(0,0,0,0.3)",
+        // padding: "2rem",
+
+        // html form label
+        "& label": {
+            color: "tomato",
+            fontSize: "1.5rem",
+            // letterSpacing: "0.125rem",
         },
-        "& h1": { ...theme.typography.h1 },
-        "& h2": { ...theme.typography.h2 },
-        "& h3": { ...theme.typography.h3 },
-        "& h4": { ...theme.typography.h4 },
-        "& h5": { ...theme.typography.h5 },
-        "& h6": {
-            ...theme.typography.h6,
-            // fontFamily: "PT Sans Narrow",
-            // fontWeight: 400,
-            // textShadow:
-            //     "3px 3px 10px rgba(255,0,0,0.5), -3px -3px 10px rgba(0,0,255,0.5)",
+
+        // MatUI form label
+        "& .MuiFormLabel-root": {
+            // color: "red",
+            fontSize: "0.875rem",
+            // letterSpacing: "0.1rem",
         },
-        "& p": {
-            ...theme.html.paragraph,
+        // label focused styles - when it moves up
+        "& label.Mui-focused": {
+            color: "green",
         },
-        "& .alignleft": {
-            float: "left",
-            margin: "1rem",
-            marginRight: "2.5rem",
-            marginLeft: 0,
-            display: "flow-root",
+
+        // input and textarea fields
+        "& .MuiTextField-root": {
+            backgroundColor: "white",
+            marginTop: 0,
+            fontSize: "0.875rem",
+            "&>*": {
+                fontSize: "inherit",
+            },
+
+            // marginBottom: theme.spacing(4),
+            // marginTop: theme.spacing(2),
+            // marginBottom: theme.spacing(4),
+            // width: "25ch",
+            // width: "100%",
         },
-        "& .alignleft:after": {
-            content: ".",
-            display: "block",
-            height: 0,
-            clear: "both",
-            visibility: "hidden",
+
+        "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+                // borderColor: "skyblue",
+            },
+            // fieldset hover styles
+            "&:hover fieldset": {
+                borderColor: "skyblue",
+                boxShadow: "0 0 5px 0px rgba(255, 255, 255, 0.3)",
+                // backgroundColor: "#eee2",
+            },
+            // fieldset focused styles
+            "&.Mui-focused fieldset": {
+                borderColor: "green",
+                borderWidth: "1px",
+                color: "yellow",
+            },
         },
-        "& .gatsby-image-wrapper ": {
-            border: "10px solid #fff",
-            boxShadow: "3px 3px 10px 3px rgba(0,0,0,0.2)",
-            // width: "30% !important",
-            // boxShadow:
-            //     " 3px 3px 10px 2px rgba(255,0,0,0.5), -3px -3px 10px 2px rgba(0,0,255,0.5)",
+        // input field typed symbols color
+        "& input": { color: "rgba(255, 0, 255, 0.8)" },
+
+        // textarea field typed symbols color
+        // "& .MuiOutlinedInput-inputMultiline": { color: "yellow" },
+        "& textarea": {
+            color: "rgba(0, 0, 255, 0.8)",
         },
+        "& .MuiFormHelperText-root": {
+            color: "tomato",
+        },
+    },
+    error: {
+        color: "lime",
     },
 }))
 
+const getIntlStringById = (id) => {
+    return injectIntl(({ intl }) => {
+        // return intl.locale
+        return intl.formatMessage({ id: id })
+    })()
+}
+
+const schema = yup.object().shape({
+    // firstName: yup.string().required(),
+    // age: yup.number().positive().integer().required(),
+    email: yup
+        .string()
+        // .required(getIntlStringById("contactFormErrorEnterEmailAddressPlease"))
+        .required("contactFormErrorEnterEmailAddressPlease")
+        .email(
+            "contactFormErrorWeNeedYourEmailAddressToContactYou"
+            // getIntlStringById(
+            //     "contactFormErrorWeNeedYourEmailAddressToContactYou"
+            // )
+        ),
+    name: yup
+        .string()
+        // .required(getIntlStringById("contactFormErrorFieldIsRequired"))
+        .required("contactFormErrorFieldIsRequired")
+        // .min(8, getIntlStringById("contactFormErrorMessageMustBeAtLeast8")),
+        .min(8, "contactFormErrorMessageMustBeAtLeast8"),
+})
+
 const ContactsPageView = ({
+    intl,
     className,
     heading,
     emailLabel,
@@ -69,42 +152,192 @@ const ContactsPageView = ({
     phoneNumberLabel,
     phone,
 }) => {
+    const { register, errors, handleSubmit, clearErrors, control } = useForm({
+        // validateCriteriaMode: "firstErrorDetected",
+        validateCriteriaMode: "all",
+        // mode: "onChange",
+        mode: "onBlur",
+        reValidateMode: "onChange", //onChange
+        defaultValues: {
+            name: "",
+            phone: "",
+            email: "",
+        },
+        resolver: yupResolver(schema),
+    })
+
+    const onSubmit = (data) => {
+        // event.preventDefault()
+        console.log(data)
+    }
+    //https://github.com/react-hook-form/react-hook-form/issues/283
+    const clearInputError = useCallback(
+        (event) => {
+            if (errors[event.target.name]) {
+                clearErrors(event.target.name)
+            }
+        },
+        [clearErrors, errors]
+    )
+
     const classes = useStyles()
+    const theme = useTheme()
 
     return (
-        <Grid
-            container
-            spacing={4}
+        <Container
+            maxWidth={theme.siteContainer.maxWidth}
             // {...rest}
             className={clsx(classes.root, className)}
         >
-            <Grid item xs={12}>
-                <Typography
-                    className={classes.header}
-                    variant="h1"
-                    component="h1"
-                    align="center"
-                >
-                    {heading}
-                </Typography>
-                <h4>
-                    {emailLabel}: {email}
-                </h4>
-                <h4>{addressLabel}:</h4>
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: address,
-                    }}
-                />
-                <h4>
-                    {phoneNumberLabel}: {phone}
-                </h4>
+            <Grid container spacing={4}>
+                <Grid item xs={12}>
+                    {/* <DevTool control={control} /> */}
+                    <Typography
+                        className={classes.header}
+                        variant="h3"
+                        component="h3"
+                        align="center"
+                    >
+                        {heading}
+                    </Typography>
+                </Grid>
             </Grid>
-        </Grid>
+
+            <Grid container spacing={4}>
+                <Grid item xs={12} md={7}>
+                    <form
+                        className={classes.form}
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    variant="outlined"
+                                    size="small"
+                                    margin="normal"
+                                    // required
+                                    fullWidth
+                                    name="name"
+                                    label="Your Name"
+                                    type="text"
+                                    id="name"
+                                    autoComplete="off"
+                                    className={classes.textField}
+                                    // error={errors.name}
+                                    inputRef={register}
+                                    // autoFocus
+                                    onChange={clearInputError}
+                                />
+                                {
+                                    <div className={classes.error}>
+                                        {errors.name &&
+                                            intl.formatMessage({
+                                                id: errors.name.message,
+                                            })}
+                                    </div>
+                                }
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    variant="outlined"
+                                    size="small"
+                                    margin="normal"
+                                    // required
+                                    fullWidth
+                                    name="email"
+                                    label="Your EMail"
+                                    type="email"
+                                    id="email"
+                                    autoComplete="off"
+                                    className={classes.textField}
+                                    // error={!!errors.phone}
+                                    inputRef={register}
+                                    onChange={clearInputError}
+                                />
+                                {
+                                    <div className={classes.error}>
+                                        {errors.email &&
+                                            intl.formatMessage({
+                                                id: errors.email.message,
+                                            })}
+                                    </div>
+                                }
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    size="small"
+                                    margin="normal"
+                                    // required
+                                    fullWidth
+                                    name="email"
+                                    label="Your EMail"
+                                    type="email"
+                                    id="email"
+                                    autoComplete="off"
+                                    className={classes.textField}
+                                    // error={!!errors.phone}
+                                    inputRef={register}
+                                    onChange={clearInputError}
+                                />
+                                {
+                                    <div className={classes.error}>
+                                        {errors.email &&
+                                            intl.formatMessage({
+                                                id: errors.email.message,
+                                            })}
+                                    </div>
+                                }
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    size="small"
+                                    margin="normal"
+                                    multiline
+                                    rows={7}
+                                    rowsMax={12}
+                                    // required
+                                    fullWidth
+                                    name="email"
+                                    label="Your EMail"
+                                    type="email"
+                                    id="email"
+                                    autoComplete="off"
+                                    className={classes.textField}
+                                    // error={!!errors.phone}
+                                    inputRef={register}
+                                    onChange={clearInputError}
+                                />
+                                {
+                                    <div>
+                                        {errors.email &&
+                                            intl.formatMessage({
+                                                id: errors.email.message,
+                                            })}
+                                    </div>
+                                }
+                            </Grid>
+                        </Grid>
+                        <Button
+                            variant="contained"
+                            type="submit"
+                            disabled={!!errors.name || !!errors.email}
+                            className={classes.submit}
+                        >
+                            Send
+                        </Button>
+                    </form>
+                </Grid>
+                <Grid item xs={12} md={5} container>
+                    <div className={classes.rightCol}></div>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
-export default ContactsPageView
+export default injectIntl(ContactsPageView)
 
 ContactsPageView.propTypes = {
     className: PropTypes.string,
